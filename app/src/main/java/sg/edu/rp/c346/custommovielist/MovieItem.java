4238a7c6456675d976f616e1646af95e7c61d843
WANG YULONG <16003749@rp.edu.sg>
2018-07-16 12:14:17 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/custommovielist/MovieItem.java
@@ -0,0 +1,39 @@
+package sg.edu.rp.c346.custommovielist;
+
+/**
+ * Created by 16003749 on 16/7/2018.
+ */
+
+public class MovieItem {
+    private  String movieName;
+    private  String releaseDate;
+
+    public MovieItem(String movieName, String releaseDate){
+        this.movieName=movieName;
+        this.releaseDate=releaseDate;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "MovieItem{" +
+                "movieName='" + movieName + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                '}';
+    }
+}
